c5842b72ba2f475cdcb8f0450db3a3ad68f50ef1
niklas <anenico2007@gmail.com>
2019-03-20 11:40:03 +0100

    ajout de datafilms.js

---
+++ components/DataFilms.js
@@ -0,0 +1,190 @@
+export const filmsDataList=  [
+    {
+        "id": 1,
+        "titre": "The Fast and the Furious",
+        "date de sortie": "26 September 2001",
+        "réalisateur": "Rob Cohen",
+        "acteurs principaux": [
+            "Vin Diesel",
+            "Paul Walker",
+            "Michelle Rodriguez"
+        ],
+        "résumé": "La nuit tombée, Dominic Toretto règne sur les rues de Los Angeles à la tête d'une équipe de fidèles qui partagent son goût du risque, sa passion de la vitesse et son culte des voitures de sport lancées à plus de 250 km/h dans des rodéos urbains d'une rare violence. Ses journées sont consacrées à bricoler et à relooker des modèles haut de gamme, à les rendre toujours plus performants et plus voyants, à organiser des joutes illicites où de nombreux candidats s'affrontent sans merci sous le regard énamouré de leurs groupies. A la suite de plusieurs attaques de camions, la police de L.A. décide d'enquêter sur le milieu des street racers. Brian, un jeune policier, est chargé d'inflitrer la bande de Toretto, qui figure, avec celle de son rival Johnny Tran, au premier rang des suspects. ",
+        "genre": [
+            "Action",
+            "Policier"
+        ],
+        "nationalité": "Américain",
+        "durée": "1h 47min"
+    },
+    {
+        "id": 2,
+        "titre": "Sexy Dance 2",
+        "date de sortie": "9 avril 2008",
+        "réalisateur": "Jon M. Chu",
+        "acteurs principaux": [
+            "Robert Hoffman",
+            "Briana Evigan",
+            "Cassie Ventura"
+        ],
+        "résumé": "Andie est une fille d'origine modeste, une rebelle qui s'efforce de trouver sa place au sein de la très respectable Maryland School Of The Arts, sans renier pour autant ses racines et son vieux rêve : intégrer la troupe Underground 410 qui rassemble les meilleurs danseurs de rue de Baltimore. Chase est l'étudiant le plus brillant de la MSA - une star en devenir qui aspire à rompre avec les traditions et contraintes de la danse classique. Son but : monter sa propre équipe pour affronter la 410 dans une grande \"bataille\" de rueIrrésistiblement attirés l'un vers l'autre, Chase et Andie arriveront-ils à concilier leurs ambitions respectives, leurs rêves et leurs désirs ? ",
+        "genre": [
+            "Comédie dramatique",
+            "Musical",
+            "Romance"
+        ],
+        "nationalité": "Américain",
+        "durée": "1h 42min"
+    },
+    {
+        "id": 3,
+        "titre": "Intouchables",
+        "date de sortie": "2 novembre 2011",
+        "réalisateur": [
+            "Eric Toledano",
+            "Olivier Nakache"
+        ],
+        "acteurs principaux": [
+            "François Cluzet",
+            "Omar Sy",
+            "Anne Le Ny"
+        ],
+        "résumé": "A la suite d’un accident de parapente, Philippe, riche aristocrate, engage comme aide à domicile Driss, un jeune de banlieue tout juste sorti de prison. Bref la personne la moins adaptée pour le job. Ensemble ils vont faire cohabiter Vivaldi et Earth Wind and Fire, le verbe et la vanne, les costumes et les bas de survêtement... Deux univers vont se télescoper, s’apprivoiser, pour donner naissance à une amitié aussi dingue, drôle et forte qu’inattendue, une relation unique qui fera des étincelles et qui les rendra... Intouchables. ",
+        "genre": [
+            "Comédie"
+        ],
+        "nationalité": "Français",
+        "durée": "1h 52min"
+    },
+    {
+        "id": 4,
+        "titre": "Qu'est-ce qu'on a fait au Bon Dieu ?",
+        "date de sortie": "16 avril 2014",
+        "réalisateur": "Philippe de Chauveron",
+        "acteurs principaux": [
+            "Christian Clavier",
+            "Chantal Lauby",
+            "Ary Abittan"
+        ],
+        "résumé": "Claude et Marie Verneuil, issus de la grande bourgeoisie catholique provinciale sont des parents plutôt \"vieille France\". Mais ils se sont toujours obligés à faire preuve d'ouverture d'esprit...Les pilules furent cependant bien difficiles à avaler quand leur première fille épousa un musulman, leur seconde un juif et leur troisième un chinois. Leurs espoirs de voir enfin l'une d'elles se marier à l'église se cristallisent donc sur la cadette, qui, alléluia, vient de rencontrer un bon catholique.",
+        "genre": [
+            "Comédie"
+        ],
+        "nationalité": "Français",
+        "durée": "1h 37min"
+    },
+    {
+        "id": 5,
+        "titre": "Blade Runner",
+        "date de sortie": "15 septembre 1982",
+        "réalisateur": "Ridley Scott",
+        "acteurs principaux": [
+            "Harrison Ford",
+            "Rutger Hauer",
+            "Sean Young"
+        ],
+        "résumé": "Dans les dernières années du 20ème siècle, des milliers d'hommes et de femmes partent à la conquête de l'espace, fuyant les mégalopoles devenues insalubres. Sur les colonies, une nouvelle race d'esclaves voit le jour : les répliquants, des androïdes que rien ne peut distinguer de l'être humain. Los Angeles, 2019. Après avoir massacré un équipage et pris le contrôle d'un vaisseau, les répliquants de type Nexus 6, le modèle le plus perfectionné, sont désormais déclarés \"hors la loi\". Quatre d'entre eux parviennent cependant à s'échapper et à s'introduire dans Los Angeles. Un agent d'une unité spéciale, un blade-runner, est chargé de les éliminer. Selon la terminologie officielle, on ne parle pas d'exécution, mais de retrait... ",
+        "genre": [
+            "Science fiction"
+        ],
+        "nationalité": "Américain",
+        "durée": "1h 57min"
+    },
+    {
+        "id": 6,
+        "titre": "12 Years a Slave",
+        "date de sortie": "22 janvier 2014",
+        "réalisateur": "Steve McQueen (II)",
+        "acteurs principaux": [
+            "Chiwetel Ejiofor",
+            "Michael Fassbender",
+            "Benedict Cumberbatch"
+        ],
+        "résumé": "Les États-Unis, quelques années avant la guerre de Sécession. Solomon Northup, jeune homme noir originaire de l’État de New York, est enlevé et vendu comme esclave. Face à la cruauté d’un propriétaire de plantation de coton, Solomon se bat pour rester en vie et garder sa dignité. ",
+        "genre": [
+            "Drame",
+            "Historique"
+        ],
+        "nationalité": "Américain",
+        "durée": "2h 13min"
+    },
+    {
+        "id": 7,
+        "titre": "Bad Boys II",
+        "date de sortie": "2h 27min",
+        "réalisateur": "Michael Bay",
+        "acteurs principaux": [
+            "Will Smith",
+            "Martin Lawrence",
+            "Jordi Mollà"
+        ],
+        "résumé": "Les policiers Marcus Burnett et Mike Lowrey enquêtent sur Tapia, un ambitieux baron de la drogue décidé à tout pour inonder Miami d'un nouveau poison et accroître son empire. Au cours de leurs investigations, ils se voient épaulés par Syd, la soeur de Marcus, également agent de la D.E.A. (Drug Enforcement Agency). Mike s'éprendra de la demoiselle, provoquant ainsi quelques tensions entre lui et son partenaire... ",
+        "genre": [
+            "Action",
+            "Comédie",
+            "Policier"
+        ],
+        "nationalité": "Américain",
+        "durée": "2h 27min"
+    },
+    {
+        "id": 8,
+        "titre": "Jumanji",
+        "date de sortie": "14 février 1996",
+        "réalisateur": "Joe Johnston",
+        "acteurs principaux": [
+            "Robin Williams",
+            "Kirsten Dunst",
+            "Bradley Pierce"
+        ],
+        "résumé": "Lors d'une partie de Jumanji, un jeu très ancien, le jeune Alan est propulsé sous les yeux de son amie d'enfance, Sarah, dans un étrange pays. Il ne pourra s'en échapper que lorsqu'un autre joueur reprendra la partie et le libèrera sur un coup de dés. Vingt-six ans plus tard, il retrouve le monde réel par le coup de dés de deux autres jeunes joueurs.",
+        "genre": [
+            "Fantastique",
+            "Aventure"
+        ],
+        "nationalité": "Américain",
+        "durée": "1h 40min"
+    },
+    {
+        "id": 9,
+        "titre": "21 grammes",
+        "date de sortie": "21 janvier 2004",
+        "réalisateur": "Alejandro González Iñárritu",
+        "acteurs principaux": [
+            "Sean Penn",
+            "Benicio Del Toro",
+            "Naomi Watts"
+        ],
+        "résumé": "On dit que nous perdons tous 21 grammes au moment précis de notre mort... Le poids de cinq pièces de monnaie. Le poids d'une barre de chocolat. Le poids d'un colibri. 21 grammes. Est-ce le poids de notre âme ? Est-ce le poids de la vie ? Paul attend une transplantation cardiaque. Cristina, ex-junkie, est mère de deux petites filles. Jack sort de prison et redécouvre la foi. A cause d'un accident, ils vont s'affronter, se haïr... et s'aimer. ",
+        "genre": [
+            "Drame"
+        ],
+        "nationalité": "Américain",
+        "durée": "2h 04min",
+        "lien vidéo": "http://www.allocine.fr/video/player_gen_cmedia=18359195&cfilm=47795.html"
+    },
+    {
+        "id": 10,
+        "titre": "Babel",
+        "date de sortie": "15 novembre 2006",
+        "réalisateur": "Alejandro González Iñárritu",
+        "acteurs principaux": [
+            "Brad Pitt",
+            "Cate Blanchett",
+            "Gael García Bernal"
+        ],
+        "résumé": "En plein désert marocain, un coup de feu retentit. Il va déclencher toute une série d'événements qui impliqueront un couple de touristes américains au bord du naufrage, deux jeunes Marocains auteurs d'un crime accidentel, une nourrice qui voyage illégalement avec deux enfants américains, et une adolescente japonaise rebelle dont le père est recherché par la police à Tokyo. Séparés par leurs cultures et leurs modes de vie, chacun de ces quatre groupes de personnes va cependant connaître une même destinée d'isolement et de douleur... ",
+        "genre": [
+            "Action",
+            "Policier"
+        ],
+        "nationalité": [
+            "Français",
+            "Américain",
+            "Mexicain"
+        ],
+        "durée": "2h 15min",
+        "lien vidéo": "http://www.allocine.fr/video/player_gen_cmedia=18704777&cfilm=20151.html",
+        "lien image": "https://m.media-amazon.com/images/M/MV5BYTJkZDljNGYtNjRiNC00ZmY2LTg1NmItYTI1MTllNDQzMWVmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"
+    }
+]
